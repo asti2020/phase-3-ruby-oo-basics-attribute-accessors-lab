e2038a00f0182a043b9a3dfbadf331ef87baa3d8
Astika Luintel <astild2020@gmail.com>
2022-10-12 11:44:39 -0400

    commit tree 8ab09621a6254a0d45ac57837a3e8287b6b46ed4

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,17 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+    attr_writer :meow
+    attr_accessor :name
+
+    def name=(name)
+        @name = name
+    end
+    def meow=(meow = "meow!")
+        @meow = meow
+    end
+
+end 
+mycat = Cat.new
+mycat.name ="name"
+puts mycat.meow ="meow!"
+
